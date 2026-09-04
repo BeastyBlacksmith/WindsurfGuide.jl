de8e8487ee885bf3e6734a9c50c96162625971af
Nina Lilkendey <nina.lilkendey@stud.uni-hannover.de>
2026-09-04 23:16:45 +0200

    Add check_wind_sail function to check bounds

--- src/processing.jl
+++ src/processing.jl
@@ -9,3 +9,24 @@ using CSV
 
 # For testing purposes, we use the test dataset from a CSV file.
 df_wind = CSV.read("data/test_wind_data.csv", DataFrame)
+
+
+function check_wind_sail(sails, sail_size, i, lb_wind, ub_wind, lb_sail, ub_sail)
+    if !ismissing(sails."min_sail"[i]) && !ismissing(sails."max_sail"[i])
+        if sails."min_sail"[i] <= sail_size <= sails."max_sail"[i]
+            if sails."min_knots"[i] < lb_wind
+                lb_wind = sails."min_knots"[i]
+            end
+            if sails."max_knots"[i] > ub_wind
+                ub_wind = sails."max_knots"[i]
+            end
+        end
+        if lb_sail > sails."min_sail"[i]
+            lb_sail = sails."min_sail"[i]
+        end
+        if ub_sail < sails."max_sail"[i]
+            ub_sail = sails."max_sail"[i]
+        end
+    end
+    return lb_wind, ub_wind, lb_sail, ub_sail
+end
