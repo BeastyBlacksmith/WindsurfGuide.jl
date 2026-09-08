92ba99860ba06b4578c7456d7750fb3c0f0e13ac
Nina Lilkendey <nina.lilkendey@stud.uni-hannover.de>
2026-09-08 17:11:33 +0200

    Restructure functions for windspeed check

--- src/check_wind.jl
+++ src/check_wind.jl
@@ -21,38 +21,43 @@ function check_level(guidelines, df_wind, level, i)
     return current
 end
 
-function check_wind_speed(bodyweight, sail_size, level)
+function check_windspeed(df_wind, guidelines, lb_wind, ub_wind, lb_sail, ub_sail, level)
+    results_windspeed = String[]
+    for i in 1:size(df_wind, 1)
+        if df_wind."windspeed_10m"[i] < 4
+            push!(results_windspeed, "Wind is too low for windsurfing, consider waiting for better conditions.")
+        elseif df_wind."windspeed_10m"[i] >= 28
+            push!(results_windspeed, "Wind is too strong for windsurfing, consider waiting for better conditions.")
+        elseif 4 <= df_wind."windspeed_10m"[i] < lb_wind
+            push!(results_windspeed, "Wind is too low for your sail size, consider using a bigger sail of 
+            up to $(ub_sail) m² and check again.")
+        elseif ub_wind < df_wind."windspeed_10m"[i] < 28
+            push!(results_windspeed, "Wind is too strong for your sail size, consider using a smaller sail of 
+            at least $(lb_sail) m² and check again.")
+        else
+            current = check_level(guidelines, df_wind, level, i)
+            if current == "too low for your skill level" || current == "too strong for your skill level"
+                push!(results_windspeed, "Wind is $(current), consider waiting for better conditions.")
+            else
+                push!(results_windspeed, "Wind is $(current) for windsurfing.")
+            end
+        end
+    end
+    return results_windspeed
+end
+
+function check_wind(bodyweight, sail_size, level)
     # df_wind = collect_wind_data("Sankt Peter-Ording")
     df_wind = CSV.read("data/test_wind_data.csv", DataFrame)
     guidelines, lb_wind, ub_wind, lb_sail, ub_sail = check_bodyweight(bodyweight, sail_size)
     df_results = DataFrame()
     df_results[!, :Date] = Date.(df_wind.TIME)
     df_results[!, :Time] = Time.(df_wind.TIME)
-    results = String[]
     if !(level in ["beginner", "intermediate", "advanced"])
         error("Please enter a valid skill level: beginner, intermediate, or advanced")
     end
-    for i in 1:size(df_wind, 1)
-        if df_wind."windspeed_10m"[i] < 4
-            push!(results, "Wind is too low for windsurfing, consider waiting for better conditions.")
-        elseif df_wind."windspeed_10m"[i] >= 28
-            push!(results, "Wind is too strong for windsurfing, consider waiting for better conditions.")
-        elseif 4 <= df_wind."windspeed_10m"[i] < lb_wind
-            push!(results, "Wind is too low for your sail size, consider using a bigger sail of 
-            up to $(ub_sail) m² and check again.")
-        elseif ub_wind < df_wind."windspeed_10m"[i] < 28
-            push!(results, "Wind is too strong for your sail size, consider using a smaller sail of 
-            at least $(lb_sail) m² and check again.")
-        else
-            current = check_level(guidelines, df_wind, level, i)
-            if current == "too low for your skill level" || current == "too strong for your skill level"
-                push!(results, "Wind is $(current), consider waiting for better conditions.")
-            else
-                push!(results, "Wind is $(current) for windsurfing.")
-            end
-        end
-    end
-    df_results[!, "recommendations"] = results
+    results_windspeed = check_windspeed(df_wind, guidelines, lb_wind, ub_wind, lb_sail, ub_sail, level)
+    df_results[!, "recommendations"] = results_windspeed
     return df_results
 end
 
