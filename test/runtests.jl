49d81c482f3bfaa10f3982e623dc29413174266a
Nina Lilkendey <nina.lilkendey@stud.uni-hannover.de>
2026-09-11 12:43:52 +0200

    Add tests for sail and final recommendation and check_windspeed, restructure testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,41 +1,79 @@
 using Test
 
-@testset "directions" begin
-    result = directions(270)
+@testset "windsurfing recommendations" begin
+    @testset "sail recommendations" begin
+        guidelines1, lb_wind1, ub_wind1, lb_sail1, ub_sail1 = check_bodyweight(72, 5)
+        guidelines2, lb_wind2, ub_wind2, lb_sail2, ub_sail2 = check_bodyweight(75, 5)
 
-    @test result."dir"[2] == "crossshore"
-    @test result."start"[4] == 247.5
-    @test result."stop"[6] == 202.5
-    @test_throws ErrorException directions(360)
-    @test_throws ErrorException directions(-1)
-end
+        @test [lb_wind1, ub_wind1, lb_sail1, ub_sail1] == [4, 19, 4, 5.5]
+        @test [lb_wind2, ub_wind2, lb_sail2, ub_sail2] == [13, 28, 4.5, 6]
+    end
 
-@testset "check_direction" begin
-    df_test1 = DataFrame(
-        wind_direction_10m = [0, 90, 180, 270])
-    df_test2 = DataFrame(
-        wind_direction_10m = [337.5, 337.4, 337.6])
+    @testset "wind direction" begin
+        result = directions(270)
 
-    result1 = check_direction(df_test1, 270, "beginner")
-    result2 = check_direction(df_test2, 270, "beginner")
+        @test result."dir"[2] == "crossshore"
+        @test result."start"[4] == 247.5
+        @test result."stop"[6] == 202.5
+        @test_throws ErrorException directions(360)
+        @test_throws ErrorException directions(-1)
 
-    @test result1[1] == "Crossshore wind, very good for all levels."
-    @test result1[2] == "Offshore wind, consider waiting for better conditions."
-    @test result1[3] == "Crossshore wind, very good for all levels."
-    @test result1[4] == "Onshore wind, very good for your skill level."
-    @test result2[1] == "Crossshore wind, very good for all levels."
-    @test result2[2] == "Cross-onshore wind, very good for all levels."
-    @test result2[3] == "Crossshore wind, very good for all levels."
-end
+        df_test1 = DataFrame(
+            wind_direction_10m = [0, 90, 180, 270])
+        df_test2 = DataFrame(
+            wind_direction_10m = [337.5, 337.4, 337.6])
 
-@testset "check_gusts" begin
-    df_test = DataFrame(
-        windspeed_10m = [10, 10, 10],
-        wind_gusts_10m = [17, 18, 19])
+        result1 = check_direction(df_test1, 270, "beginner")
+        result2 = check_direction(df_test2, 270, "beginner")
 
-    result = check_gusts(df_test, "beginner")
+        @test result1[1] == "Crossshore wind, very good for all levels."
+        @test result1[2] == "Offshore wind, consider waiting for better conditions."
+        @test result1[3] == "Crossshore wind, very good for all levels."
+        @test result1[4] == "Onshore wind, very good for your skill level."
+        @test result2[1] == "Crossshore wind, very good for all levels."
+        @test result2[2] == "Cross-onshore wind, very good for all levels."
+        @test result2[3] == "Crossshore wind, very good for all levels."
+    end
 
-    @test occursin("stable", result[1])
-    @test occursin("too strong", result[2])
-    @test occursin("too strong", result[3])
+    @testset "wind conditions" begin
+        df_test = DataFrame(
+            windspeed_10m = [10, 10, 10],
+            wind_gusts_10m = [17, 18, 19])
+
+        result = check_gusts(df_test, "beginner")
+
+        @test occursin("stable", result[1])
+        @test occursin("too strong", result[2])
+        @test occursin("too strong", result[3])
+
+        guidelines = CSV.read("data/guidelines.csv", DataFrame, missingstring = "missing")
+        lb_wind = 5
+        ub_wind = 19
+        lb_sail = 4.0
+        ub_sail = 5.5
+        
+        df_tests = DataFrame(
+            windspeed_10m = [2, 4, 5, 9, 14, 18, 19, 28])
+
+        results = check_windspeed(df_tests, guidelines, "intermediate", lb_wind, ub_wind, lb_sail, ub_sail)
+
+        @test occursin("too low for windsurfing", results[1])
+        @test occursin("too low for your sail size", results[2])
+        @test occursin("too low for your skill level", results[3])
+        @test occursin("is good", results[4])
+        @test occursin("very good", results[5])
+        @test occursin("good for improvement", results[6])
+        @test occursin("too strong for your sail size", results[7])
+        @test occursin("too strong for windsurfing", results[8])
+    end
+
+    @testset "final recommendations" begin
+        df_test = get_recs_test(72, 5, "intermediate", 270)
+
+        @test occursin("very good", df_test."Recommendation"[53])
+        @test occursin("gusts are too strong", df_test."Recommendation"[52])
+        @test occursin("Offshore", df_test."Recommendation"[78])
+        @test occursin("too low for your skill level", df_test."Recommendation"[66])
+        @test occursin("too strong for your sail size", df_test."Recommendation"[144])
+    end
 end
